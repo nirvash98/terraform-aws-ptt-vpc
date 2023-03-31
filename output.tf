32a6b6f899e68a090c2e20b1b3e7b40631858efc
nirvash98 <nirvash98@yonsei.ac.kr>
2023-03-31 11:10:44 +0900

    output.tf 수정2

--- output.tf
+++ output.tf
@@ -3,9 +3,9 @@ output "vpc_id" {
 }
 
 output "public_subnet_id" {
-    value = aws_subnet.public.id
+    value = aws_subnet.public[*].id
 }
 
 output "private_subnet_id" {
-    value = aws_subnet.private.id
+    value = aws_subnet.private[*].id
 }
